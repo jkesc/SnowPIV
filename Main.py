bd190ef26b4c2b01ecc60bb039858e221cbacbb4
jkesc <59794645+jkesc@users.noreply.github.com>
2021-06-13 20:48:44 +0200

    Iterate through first 20 frames of video.
    takes hours...

--- Main.py
+++ Main.py
@@ -6,6 +6,8 @@ Created on Sun Mar 21 21:53:47 2021
 """
 #To do:
 #It seems like some parts get larger values at false locations. probably because the range is fuzzy(not 1 or 0). Should make it either snowflake or noflake, in order to avoid this.
+#Should also confine it to one size of snowflakes. At it is now. one flake close to thecamera always results in large values, so all vectors point towards it.
+#Also: maybe find a way to reduce the resoultion of the picture to get fewer pixels -> fewer iterations?
 #main function
 import FilterBG as FBG
 import GetContrast as GC
@@ -15,13 +17,14 @@ import Visualization as VS
 
 def main():
     BG=FBG.GetBG('PIV2.mp4')
-    threshold=100#contrast threshold
+    treshold=100#contrast threshold
     for i in range(20):
-        contrastMap0=GC.RemoveBG(BG,'PIV2.mp4',record=False,ContrastThreshold=threshold,filename=str(threshold)+'.avi',frame=i)
-        contrastMap1=GC.RemoveBG(BG,'PIV2.mp4',record=False,ContrastThreshold=threshold,filename=str(threshold)+'.avi',frame=i+1)
+        contrastMap0=GC.RemoveBG(BG,'PIV2.mp4',record=False,ContrastTreshold=treshold,filename=str(treshold)+'.avi',frame=i)
+        contrastMap1=GC.RemoveBG(BG,'PIV2.mp4',record=False,ContrastTreshold=treshold,filename=str(treshold)+'.avi',frame=i+1)
         dx,dy,idx=CC.crossCorelateFrames(contrastMap0[0:400,250:650],10,contrastMap1[0:400,250:650],True)
         image=VS.vectorField(contrastMap0[0:400,250:650],dx,dy,idx)
         image.savefig('vectorField_frame_'+str(i)+'.png')
+        image.clf()
     return [contrastMap0,contrastMap1,BG,dx,dy,idx]#, CorelationMap
 
 if __name__=="__main__":
